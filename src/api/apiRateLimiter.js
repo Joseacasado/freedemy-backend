58bd50e71e0a119b7c29d675a7d68167e47ef729
Joseacasado <jsedoviolt@gmail.com>
2022-05-18 23:34:32 +0200

    create and add apiRateLimiter middleware

---
+++ src/api/apiRateLimiter.js
@@ -0,0 +1,41 @@
+const rateLimit = require('express-rate-limit')
+const appName = require('../../package.json').name
+
+//  See documentation: https://www.npmjs.com/package/express-rate-limit
+
+function createRateLimiter({ windowMs, max, message}) {
+    return rateLimit({
+        windowMs,
+        max,
+        message,
+        standardHeaders: true,          // Return rate limit info in the `RateLimit-*` headers
+        requestPropertyName: appName    // The name of the property on the Express request object to store the rate limit info.
+    })
+}
+
+const defaultRateLimiter = createRateLimiter({
+    windowMs: 15 * 60 * 1000,       // 15 minutes (in milliseconds)
+    max: 500,                       // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+    message: 'Too Many Requests.',
+})
+
+module.exports = defaultRateLimiter
+
+//  TO-DO 
+//  export createRateLimiter as default
+//  export defaultRateLimiter as constant
+
+/*
+ *  If you are behind a proxy/load balancer (usually the case with most hosting services, e.g. Heroku, Bluemix, AWS ELB, Nginx, Cloudflare, 
+    Akamai, Fastly, Firebase Hosting, Rackspace LB, Riverbed Stingray, etc.), the IP address of the request might be the IP of the load 
+    balancer/reverse proxy (making the rate limiter effectively a global one and blocking all requests once the limit is reached) or undefined. 
+    To solve this issue, add the following line to your code (right after you create the express application)
+
+    app.set('trust proxy', numberOfProxies)
+
+    Where numberOfProxies is the number of proxies between the user and the server. 
+    To find the correct number, create a test endpoint that returns the client IP:
+
+    app.set('trust proxy', 1)
+    app.get('/ip', (request, response) => response.send(request.ip))
+*/
--- src/config/middleware.config.js
+++ src/config/middleware.config.js
@@ -1,8 +1,10 @@
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const express = require('express')
+const helmet = require('helmet')
 const languageMiddleware = require('../middlewares/language.middleware')
 const tokenMiddleware = require('../middlewares/token.middleware')
+const defaultRateLimiter = require('../api/apiRateLimiter')
 
 module.exports = app => {
     app.use(logger('dev'))
@@ -11,4 +13,6 @@ module.exports = app => {
     app.use(express.urlencoded({ extended: true }))
     app.use(languageMiddleware)
     app.use(tokenMiddleware)
+    app.use(defaultRateLimiter)
+    app.use(helmet())
 }
