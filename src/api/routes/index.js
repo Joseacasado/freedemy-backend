f35476258c3e6faa0ab47e9cb99b8242101b73d0
Joseacasado <jsedoviolt@gmail.com>
2022-05-24 07:40:51 +0200

    add npm standard, add npm run lint script to package.json, add npm run lint to deployment pipeline

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,5 +1,5 @@
 module.exports = app => {
-    app.use('/jose', require('./jose.routes'))
-    app.use('/marisa', require('./marisa.routes'))
-    app.use('/api', require('./api.routes'))
+  app.use('/jose', require('./jose.routes'))
+  app.use('/marisa', require('./marisa.routes'))
+  app.use('/api', require('./api.routes'))
 }
