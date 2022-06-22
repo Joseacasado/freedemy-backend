e49587018be88cc8f372753cf97cef93fbc85fd2
Joseacasado <jsedoviolt@gmail.com>
2022-06-22 09:11:49 +0200

    create openapi.json, setup swagger

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -1,39 +1,14 @@
-
-const swaggerJsdoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../documentation/openapi.json')
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Freedemy',
-      version: 2.0,
-      description: 'This is a simple CRUD API application made with Express and documented with Swagger'
-    },
-    // securityDefinitions: {   //if it is not an OpenApi
-    //   access_token: {
-    //     type: 'apiKey',
-    //     name: 'x-access-token',
-    //     in: 'header'
-    //   }
-    // },
-    // security: [
-    //   {
-    //     access_token: []
-    //   }
-    // ]
-    servers: [
-      {
-        url: 'http://localhost:5050/marisa'
-      }
-    ]
-
-  },
-
-  apis: ['./api/routes/*.js'] // Path to the API docs
-
-}
-const specs = swaggerJsdoc(options)
 module.exports = app => {
-  app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }))
+  if (String(process.env.API_DOCUMENTATION_ENABLED) !== 'true') {
+    return
+  }
+
+  const swaggerUiOptions = {
+    explorer: true
+  }
+
+  app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerUiOptions))
 }
